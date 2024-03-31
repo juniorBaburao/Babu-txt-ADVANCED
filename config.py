d0a3da8a2238ddfd8245b4610b6b7efb43ce4f2a
juniorBaburao <163979343+juniorBaburao@users.noreply.github.com>
2024-04-01 02:48:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = API_ID = 22435461
 API_HASH = os.environ.get("API_HASH", "fe62b0736218400bf1209d831b6af1a7")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "> Wow Tapatap:
-6995524576:AAEVCEcEbEvdk1eGoZ46FyrEH9rCyTCxCjU
+7166121629:AAHLN3Xe548E7BcpmIcn4oqAHmJbWKHM30o
 ")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
@@ -16,7 +16,7 @@ LOG = -1002090024849
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5987970971").split()):
+    for x in (os.environ.get("ADMINS", "1459497376").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
